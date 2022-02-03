9656ca774dc23773c9319a4e324d343e3ba30e4b
Nikitadivan <sarapul-mebel@mail.ru>
2022-02-03 23:50:41 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30;
+        int bonus = 20;
+
+        int allBonus = ticketPrice/bonus;
+        System.out.println(allBonus);
+
+    }
+}
